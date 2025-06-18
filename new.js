4c835cfeb335c97e98dc2e6b1741ff744386ca23
Parvath Reddy <136846819+parvath-reddy@users.noreply.github.com>
2025-06-18 21:05:10 +0530

    day 4 (into to js and installing node.js)

---
+++ new.js
@@ -0,0 +1,3 @@
+console.log("code is running..."); //using this tool we can see the code running in the browser and in inspect console we can see it so it will help in debugging 
+console.log("code is too good running..."); 
+console.log("code is hell ya  running..."); 
---
+++ script.js
@@ -0,0 +1,19 @@
+alert("joining the js");//using this tool we can show alerts in the site 
+console.log("code is running..."); //using this tool we can see the code running in the browser and in inspect console we can see it so it will help in debugging 
+console.log("code is too good running..."); 
+console.log("code is hell ya  running..."); 
+
+var a = prompt("enter your number")
+var isTrue = confirm("are you sure you are leaving this page if you youll get vires in your pc ")
+if(isTrue){
+    console.log("the computer is virusing")
+}
+else{
+    console.log("the computer is not virusing ")
+}
+
+console.log("your numner is "+a)
+
+document.title = "my first web page using js" //in lone css using the js 
+
+document.body.style.backgroundColor = "gray"  // so using js we can also run the css codes here it is very convenient and a bit easyish and also the sintax of js and node js is completly same 
