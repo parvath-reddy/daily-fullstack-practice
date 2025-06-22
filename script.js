f2f44f2d7a269b096031189df144ffbdf8284ac7
Parvath Reddy <136846819+parvath-reddy@users.noreply.github.com>
2025-06-22 19:36:17 +0530

    day 17

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-//these are the premative data types 
-let x = " ankoor goije";
-let y = 22;
-let z = 69.69;
-const r = true
-let q = undefined;
-let n = null;
 
-console.log(x,y,z,r,q,n);
-console.log(typeof x,typeof y,typeof z,typeof r,typeof q,typeof n);
-
-//this are the opjects 
-
-let g = {
-    name : "ankoor",
-    age : 22,
-    gender : "male",
-    isMarried : false
-    }
-console.log(g);
-g.salary="10000cr";
-console.log(g);
-g.salary="50000cr";
-console.log(g);
+//this is a way to get a single element and changing it 
+let boxes = document.getElementsByClassName("box");
+console.log(boxes);
+boxes[2].style.backgroundColor = "green";
 
 
- 
+document.getElementById("z").style.backgroundColor = "blue"; // by creating the ids and changing the color and stuff 
+//this is used to change just one by naimg the id so it will not change the others and it is optimal 
+
+document.querySelector(".box").style.backgroundColor = "gray"; 
+// this will get the first element with the class box by using in line css in js , and only words for the first box 
+
+// this works only for the 1st element with the class box so to kmake it work for the all the elements with the class box we can use a loop
+// for (let i = 0; i < boxes.length; i++) {
+//     boxes[i].style.backgroundColor = "red"; // this will change the color of all the boxes to red
+// }
+// this is a way to get all the elements with the class box and changing them
