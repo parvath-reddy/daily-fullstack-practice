837b3c47d2b549ee35edddfab404ce923996f2cd
Parvath Reddy <136846819+parvath-reddy@users.noreply.github.com>
2025-06-23 09:26:22 +0530

    day 18

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
+console.log("script.js loaded");
+// let box = document.getElementsByClassName("box");
+// console.log(box); // This will log the HTMLCollection of elements with class "box"
 
-//this is a way to get a single element and changing it 
-let boxes = document.getElementsByClassName("box");
-console.log(boxes);
-boxes[2].style.backgroundColor = "green";
+let boxes = document.querySelector(".container").children;
 
-
-document.getElementById("z").style.backgroundColor = "blue"; // by creating the ids and changing the color and stuff 
-//this is used to change just one by naimg the id so it will not change the others and it is optimal 
-
-document.querySelector(".box").style.backgroundColor = "gray"; 
-// this will get the first element with the class box by using in line css in js , and only words for the first box 
-
-// this works only for the 1st element with the class box so to kmake it work for the all the elements with the class box we can use a loop
-// for (let i = 0; i < boxes.length; i++) {
-//     boxes[i].style.backgroundColor = "red"; // this will change the color of all the boxes to red
-// }
-// this is a way to get all the elements with the class box and changing them
+function getrandomColor() {
+    let val1=Math.ceil(0+Math.random()* 255);
+    let val2=Math.ceil(0+Math.random()* 255);
+    let val3=Math.ceil(0+Math.random()* 255);
+    return `rgb(${val1}, ${val2}, ${val3})`;
+    
+}
+Array.from(boxes).forEach(e => {
+    e.style.backgroundColor = getrandomColor();
+    e.style.Color = getrandomColor();
+})
