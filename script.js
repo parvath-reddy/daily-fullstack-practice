b4a34879d71d1ec07293791d944b507006fc8697
Parvath Reddy <136846819+parvath-reddy@users.noreply.github.com>
2025-06-24 20:44:17 +0530

    day 20

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-console.log("script.js loaded");
-// let box = document.getElementsByClassName("box");
-// console.log(box); // This will log the HTMLCollection of elements with class "box"
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+    let viewstr;
+    if (views < 1000000) {
+        viewstr = Math.floor(views / 1000) + "K";
+    } else {
+        viewstr = Math.floor(views / 1000000) + "M";
+    }
 
-let boxes = document.querySelector(".container").children;
+    let html = `
+        <div class="card">
+            <div class="image">
+                <img src="${thumbnail}" alt="thumbnail">
+                <div class="capsule">${duration}</div>
+            </div>
+            <div class="text">
+                <h1>${title}</h1>
+                <p>${cName} • ${viewstr} views • ${monthsOld} months ago</p>
+            </div>
+        </div>
+    `;
 
-function getrandomColor() {
-    let val1=Math.ceil(0+Math.random()* 255);
-    let val2=Math.ceil(0+Math.random()* 255);
-    let val3=Math.ceil(0+Math.random()* 255);
-    return `rgb(${val1}, ${val2}, ${val3})`;
-    
+    document.querySelector(".container").innerHTML += html;
 }
-Array.from(boxes).forEach(e => {
-    e.style.backgroundColor = getrandomColor();
-    e.style.Color = getrandomColor();
-})
+
+// Call the function properly (no dot at the end!)
+createCard(
+    "Introduction to Backend | Sigma Web Dev #2",
+    "CodeWithHarry",
+    540000,
+    7,
+    "31:22",
+    "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDf-xrqgUw55JSfKsoykKVhngkzRA"
+);
