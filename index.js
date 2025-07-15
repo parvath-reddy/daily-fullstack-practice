c1e03e598e6bf44e79883f298518641f47d076af
Parvath Reddy <136846819+parvath-reddy@users.noreply.github.com>
2025-07-15 23:47:32 +0530

    day 43 -(what,how,where,hosting)
    (what,how,where,hosting)

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = 3000
 
-app.set("view engine","ejs")
-app.get('/', (req, res) => {
-    let siteName = "adidas"
-    let serchText ="search now"
-    let arr = ["hay",54,65]
-  res.render("index",{siteName:siteName ,searchText:serchText,arr})
+app.get('/', (req,res) => {
+    res.send('hello world')
 })
-
-app.get('/blog/:slug', (req, res) => {
-    let blogTitle = "adidas why and when"
-    let blogContent ="its a very good brand"
-    
-  res.render("blogpost.html",{blogTitle :blogTitle , blogContent: blogContent})
-})
-
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log('example appp listoning on port ${}port');
+    
 })
